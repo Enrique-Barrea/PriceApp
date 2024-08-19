b8936a7de1b7056d7ba83922bedfe7ce5106c69a
Enrique Barrera <ebarrera1971@gmail.com>
2024-08-19 03:49:11 +0200

    Modificada SecurityConfig

--- src/main/java/pfg/priceapp/controller/LoginController.java
+++ src/main/java/pfg/priceapp/controller/LoginController.java
@@ -1,15 +1,13 @@
 package pfg.priceapp.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/pfg/priceapp/model/User.java
+++ src/main/java/pfg/priceapp/model/User.java
@@ -1,6 +1,10 @@
 package pfg.priceapp.model;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id; // Add this import statement
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "Users")
